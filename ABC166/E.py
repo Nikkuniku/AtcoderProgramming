63d9a789eca4dcab2d9e971f2f11ddd0db54181e
Nikkuniku <ymdysk911@gmail.com>
2020-05-04 19:21:41 +0900

    ABC

--- ABC166/E.py
+++ ABC166/E.py
@@ -1,6 +1,32 @@
 N=int(input())
 Agents=list(map(int,input().split()))
 
+L={}
+for i in range(N):
+    s = i+1 + Agents[i] 
+    if s in L:
+        L[s]+=1
+    else:
+        L[s]=1
+
+R={}
+for j in range(N):
+    s= j+1 - Agents[j]
+    if s in R:
+        R[s]+=1
+    else:
+        R[s]=1
+
+
+cnt=0
+for key,value in L.items():
+    cnt += R.get(key,0) * value
+
+print(cnt)
+
+
+
+##Miss 1
 # from itertools import combinations
 
 # comb = list(combinations([i for i in range(N)],2))
@@ -12,15 +38,13 @@ Agents=list(map(int,input().split()))
 #         cnt+=1
 
 # print(cnt)
-cnt=0
 
-for i in range(0,N):
-    k=Agents[i]-(i+1)
-    if k<i:
-        k=i+1
-    for j in range(k,N):
-        if Agents[i]+Agents[j]==abs(i-j):
-            # print(i,j,Agents[i],Agents[j])
-            cnt+=1
-
-print(cnt)
+# Miss 2
+# for i in range(0,N):
+#     k=Agents[i]-(i+1)
+#     if k<i:
+#         k=i+1
+#     for j in range(k,N):
+#         if Agents[i]+Agents[j]==abs(i-j):
+#             # print(i,j,Agents[i],Agents[j])
+#             cnt+=1
