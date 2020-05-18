919432fb6ac32e83a19e9b18c4884d7626880ff2
Nikkuniku <ymdysk911@gmail.com>
2020-05-18 22:28:18 +0900

    ABC

--- ABC168/D.py
+++ ABC168/D.py
@@ -1,38 +1,39 @@
-n,m  =map(int,input().split())
+n,m = map(int,input().split())
 
-root=[[pow(10,9)]*n for _ in range(n) ]
+root = [[pow(10,9)]*n for _ in range(n)]
+
+from collections import deque
+
+root=[0]*n
+r=[[]for i in range(n)]
 
 for _ in range(m):
     a,b = map(int,input().split())
+    a-=1
+    b-=1
+    r[a].append(b)
+    r[b].append(a)
 
-    root[a-1][b-1] = 0
+dist =[-1]*n
+dist[0] = 0
 
-now= 0
+q = deque([0])
+prev=[-1] * n
 
-sirube = [0]*n
+while q:
+    v = q.popleft()
+
+    for j in r[v]:
+        if dist[j]!=-1:
+            continue
+        else:
+            dist[j] = dist[v] + 1
+            prev[j] = v
+            q.append(j)
+
 
 for i in range(n):
-
-    distance=float('inf')
-
-    for j in range(len(root)):
-
-        if distance>root[i][j]:
-
-            distance = root[i][j]
-
-            sirube[j] = i
-
-            tmp=j
-
-    now =tmp
-    
-    sirube[j] = i
-
-
-
-
-
-print(sirube)
-
-print(*root,sep="\n")
+    if i==0:
+        print('Yes')
+    else:
+        print(prev[i]+1)
