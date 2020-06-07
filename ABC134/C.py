3900bad15d2c948feda0a994626eaadfeccbf5b2
Nikkuniku <ymdysk911@gmail.com>
2020-06-07 20:22:08 +0900

    ABC

--- ABC134/C.py
+++ ABC134/C.py
@@ -6,37 +6,16 @@ for i in range(N):
     a= int(input())
     seq.append(a)
 
-numbers=set(seq)
+import heapq
 
-A_max=list(numbers)[-1]
+tmp=list(map(lambda x: x*(-1),seq))
+heapq.heapify(tmp)
 
-if N>=2:
-    if len(numbers)>=2:
-        A_second = list(numbers)[-2]
+a=heapq.heappop(tmp)*(-1)
+b=heapq.heappop(tmp)*(-1)
+
+for j in range(N):
+    if seq[j]==a:
+        print(b)
     else:
-        A_second = A_max
-
-for k in range(N):
-    if seq[k]==A_max:
-        print(A_second)
-    else:
-        print(A_max)
-
-# 2.両端から
-# left=[0]
-# for i in range(N-1):
-#     left_i=max(left[i],seq[i])
-
-#     left.append(left_i)
-
-# right=[0]
-
-# for j in range(N-1):
-#     right_j = max(right[j],list(reversed(seq))[j])
-
-#     right.append(right_j)
-
-# right = list(reversed(right))
-
-# for k in range(N):
-#     print(max(left[k],right[k]))
+        print(a)
