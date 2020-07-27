ca96c77890b06918be9a3e1bcc1f5c16259f90cc
Nikkuniku <ymdysk911@gmail.com>
2020-07-27 22:13:24 +0900

    Other

--- Other/Petrozavodsk001/B.py
+++ Other/Petrozavodsk001/B.py
@@ -3,4 +3,17 @@ a=list(map(int,input().split()))
 b=list(map(int,input().split()))
 
 
-print(a,b)
+c1=0
+c2=0
+
+from math import floor
+for i in range(n):
+    if a[i]>b[i]:
+        c1+=a[i]-b[i]
+    elif a[i]<b[i]:
+        c2+=floor((b[i]-a[i])/2)
+
+if c2>=c1:
+    print('Yes')
+else:
+    print('No')
