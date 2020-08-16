da5a277a23e569cc382f9cfe32420524765c0a17
Nikkuniku <ymdysk911@gmail.com>
2020-08-16 20:51:00 +0900

    ABC

--- ABC/ABC175/C.py
+++ ABC/ABC175/C.py
@@ -1,39 +1,16 @@
 x,k,d=map(int,input().split())
 
+x=abs(x)
+cnt=abs(x)//d
 
-if d>=abs(x):
-    if k%2==0:
-        p=abs(x)
-    else:
-        if x>=0:
-            p=abs(x-d)
-        else:
-            p=abs(x+d)
-    print(p)
-    exit(0)
+
+if cnt>=k:
+    print(x-d*k)
 else:
-    cnt=abs(x)//d
-
-    if cnt>=k:
-        cnt=k
-
-    if x>=0:
-        p=x-d*cnt
-    else:
-        p=x+d*cnt
-
     k-=cnt
 
-    if k==0:
-        print(abs(p))
-        exit(0)
-
     if k%2==0:
-        print(abs(p))
+        print(abs(x-d*cnt))
     else:
-        if p>=0:
-            p=abs(p-d)
-        else:
-            p=abs(p+d)
+        print(abs(x-d*(cnt+1)))
 
-        print(p)
