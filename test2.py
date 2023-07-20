ea063b11c72d7d41c9d49d881149fe6149015d6e
Nikkuniku <ymdysk911@gmail.com>
2023-07-20 18:11:11 +0900

    整理

--- test2.py
+++
@@ -1,8 +0,0 @@
-for a in range(3):
-    for b in range(3):
-        if a == b:
-            continue
-        for c in range(3):
-            if a == c or b == c:
-                continue
-            print(a, b, c)
--- ビットシフト練習.py
+++
@@ -1,6 +0,0 @@
-word=['q','w','y','p','d','f','j','l','z','x','v']
-
-for i in range(len(word)):
-    for j in range(i+1,len(word)):
-        ans='a'+word[i]+'o'+word[j]+'t'
-        print(ans)
